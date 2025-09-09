1e95343a5c869ca13579dc8e10817e396b212392
Zaher ABBAS <z.abbas83@yahoo.com>
2025-09-09 11:57:37 +0200

    add footer component and integrate it across the app; revise layout by replacing `<header>` with `<main>` for better structure; include footer in the app shell and enhance global styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {Navbar} from './components/navbar/navbar';
+import {Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {App} from './app/app';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
